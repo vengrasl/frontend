1d0175b6019f81a01ac730349d6f9ffe30e6ab57
vengrasl <vengrasl@gmail.com>
2023-01-01 15:38:53 +0200

    8,9

--- atsiskaitymai/Js - bandomasis/task8/main.js
+++ atsiskaitymai/Js - bandomasis/task8/main.js
@@ -5,3 +5,54 @@
     metodas3 priima skaičių ir grąžina true jeigu šis yra lyginis ir false kitu atveju.
     metodas4 priima žodį ir grąžina true jeigu šis yra ilgesnis nei 8 raidžių ir false kitu atveju.
 */
+
+
+class trueFalse {
+  constructor(number, string) {
+    this.number = number;
+    this.string = string;
+  }
+ methodOne(someNumber){
+   if (this.number > 100) {
+    return true
+   } else {
+    return false
+   }
+  }
+  methodTwo(someString){
+    if (this.string.charAt(0) == this.string.charAt(0).toUpperCase(0)){
+      return true
+    } else {
+      return false
+    }    
+  }
+  methodThree(someNumber){
+    if(this.number % 2 == 0){
+      return true
+    } else {
+      return false
+    }
+  }
+  methodFor(someString){
+    if (this.string.length > 8){
+      return true
+    } else{
+      return false
+    }
+  }
+}
+
+const object = new trueFalse (1000, "ilgas žodis");
+
+console.log(object.methodOne())
+
+console.log(object.methodTwo())
+
+console.log(object.methodThree())
+
+console.log(object.methodFor())
+
+
+
+
+
--- atsiskaitymai/Js - bandomasis/task9/main.js
+++ atsiskaitymai/Js - bandomasis/task9/main.js
@@ -5,3 +5,30 @@
     1 metodas tikrina ar mašina brangesnė negu 10 000 eurų ir grąžina true|false.
     2 metodas tikrina ar mašina senesnė negu 2010 ir grąžina mašinos amžių ir tekstą "Antikvaras"|"Šviežiena".
 */
+
+class Car{
+  constructor(masinosMarke, masinosModelis, masinosKaina, masinosGamybosMetai){
+    this.masinosMarke = masinosMarke;
+    this.masinosModelis = masinosModelis;
+    this.masinosKaina = masinosKaina;
+    this.masinosGamybosMetai = masinosGamybosMetai;
+  }
+  methodOne(){
+    if (this.masinosKaina > 10000){
+    return true
+    }else {
+      return false
+    }
+  }
+  methodTwo(){
+    if (this.masinosGamybosMetai < 2010){
+    return "Antikvaras"
+  }else{
+    return "Šviežiena"
+  }
+  }
+}
+let car = new Car("toyota", "verso", 9000, 2018);
+
+console.log(car.methodOne())
+console.log(car.methodTwo())

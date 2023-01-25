64eebd09b1f445322fb83697b186e54372101665
vengrasl <vengrasl@gmail.com>
2023-01-25 11:39:32 +0200

    prisijungimas iki duomenu saugojimo

---
+++ React/57 paskaita. user verification/verification/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import Login from "./Login";
+
+const Header = () => {
+
+  return ( 
+    <>
+    <Login />
+    </>
+   );
+}
+ 
+export default Header;
--- React/57 paskaita. user verification/verification/src/index.js
+++ React/57 paskaita. user verification/verification/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { UserProvider } from './contexts/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <UserProvider>
+        <App />
+    </UserProvider>
 );

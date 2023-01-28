6fcb2056e63de578e654d35c7a141d7835bc12e4
vengrasl <vengrasl@gmail.com>
2023-01-28 12:24:39 +0200

    post by postOwner

--- React/58 paskaita... praktika/praktika/src/Post.jsx
+++ React/58 paskaita... praktika/praktika/src/Post.jsx
@@ -1,10 +1,38 @@
+import UserContext from "./UserContext";
+import { useContext } from "react";
+
 const Post = ({data}) => {
+  
+const { users, loggedInUser } = useContext(UserContext);
+
+//randam kuris postas kuriam vartotojui priklauso prilyginant vartotojo id ir posto savininko id
+const postOwner = users.find(user => user.id === data.userId);
+console.log(postOwner)
+
+
   return ( 
+
+
     <div className="singleMealContainer">
+      
+      {
+        <div className="postOwner">
+        <p>This post belongs to user: {postOwner.username}</p>
+        <img src={postOwner.avatar} alt="user avatar" />
+        </div>
+      }
+      
       <div className="singleMeal">
         <img className="mealImage" src={data.image} alt="meal-img" />
         <h1 className="mealTitle">{data.title}</h1>
         <p className="mealDescription">{data.description}</p>
+        <div>
+
+        {
+          loggedInUser && loggedInUser.id === postOwner.id && 'edit'
+        }
+          
+        </div>
       </div>
     </div>
    );
